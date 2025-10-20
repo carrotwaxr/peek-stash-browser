f4187bfef91c4017bf301ef2442e151213044f7c
Carrot Waxxer <carrotwaxr@gmail.com>
2025-10-19 22:22:43 -0700

    fix: Add missing PageLayout import to Settings page

--- client/src/components/pages/Settings.jsx
+++ client/src/components/pages/Settings.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { usePageTitle } from "../../hooks/usePageTitle.js";
+import { PageLayout } from "../ui/index.js";
 import CarouselSettings from "../settings/CarouselSettings.jsx";
 
 const api = axios.create({
