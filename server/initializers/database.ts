12ca0f870ae23f2435e05c1e07aeeb917dca3325
Carrot Waxxer <carrotwaxr@gmail.com>
2025-11-04 13:34:59 -0800

    fix imports missing extensions

--- server/initializers/database.ts
+++ server/initializers/database.ts
@@ -1,6 +1,6 @@
 import { exec } from "child_process";
 import { promisify } from "util";
-import { logger } from "../utils/logger";
+import { logger } from "../utils/logger.js";
 
 const execAsync = promisify(exec);
 
