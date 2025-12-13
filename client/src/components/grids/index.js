c76d58612d28683ccd12f70cc8bf411b15da8b9b
carrotwaxr <98351562+carrotwaxr@users.noreply.github.com>
2025-12-12 20:22:04 -0800

    feat(grids): add TagGrid component

--- client/src/components/grids/index.js
+++ client/src/components/grids/index.js
@@ -2,3 +2,4 @@ export { default as GalleryGrid } from "./GalleryGrid.jsx";
 export { default as GroupGrid } from "./GroupGrid.jsx";
 export { default as PerformerGrid } from "./PerformerGrid.jsx";
 export { default as StudioGrid } from "./StudioGrid.jsx";
+export { default as TagGrid } from "./TagGrid.jsx";
