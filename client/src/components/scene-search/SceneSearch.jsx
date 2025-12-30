472bd0008f9a45bb04531a36eb265a00055d64ac
carrotwaxr <98351562+carrotwaxr@users.noreply.github.com>
2025-12-30 11:07:06 -0800

    fix: remove unused eslint-disable directive

--- client/src/components/scene-search/SceneSearch.jsx
+++ client/src/components/scene-search/SceneSearch.jsx
@@ -32,7 +32,7 @@ const SceneSearch = ({
 }) => {
   const navigate = useNavigate();
   const location = useLocation();
-  const [searchParams] = useSearchParams();  // eslint-disable-line no-unused-vars
+  const [searchParams] = useSearchParams();
 
   const columns = useGridColumns("scenes");
 
