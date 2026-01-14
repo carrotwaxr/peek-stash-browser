b81ad4fc3c2ff357f6b76a692b2b1c52b4b5fab9
carrotwaxr <98351562+carrotwaxr@users.noreply.github.com>
2026-01-13 16:15:31 -0800

    feat: add TableView component and index exports

---
+++ client/src/components/table/index.js
@@ -0,0 +1,5 @@
+export { default as TableView } from "./TableView.jsx";
+export { default as TableHeader } from "./TableHeader.jsx";
+export { default as MultiValueCell } from "./MultiValueCell.jsx";
+export { default as ColumnConfigPopover } from "./ColumnConfigPopover.jsx";
+export * from "./cellRenderers.jsx";
