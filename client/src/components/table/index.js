e34e9e1231ad5e3b1d4de547d2bc0948a279e7b3
carrotwaxr <98351562+carrotwaxr@users.noreply.github.com>
2026-01-13 16:39:34 -0800

    refactor: move formatters to separate file to fix eslint

--- client/src/components/table/index.js
+++ client/src/components/table/index.js
@@ -3,3 +3,4 @@ export { default as TableHeader } from "./TableHeader.jsx";
 export { default as MultiValueCell } from "./MultiValueCell.jsx";
 export { default as ColumnConfigPopover } from "./ColumnConfigPopover.jsx";
 export * from "./cellRenderers.jsx";
+export * from "./formatters.js";
