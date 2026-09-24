faef919ab97aa0adbd842ebe013ece707eb1dcf9
carrotwaxr <carrotwaxr@users.noreply.github.com>
2026-09-24 03:42:09 -0700

    test: client type-error probe (to be dropped)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App";
 import "./themes/fonts";
 import "./index.css";
 
+const _probe: number = "x";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
