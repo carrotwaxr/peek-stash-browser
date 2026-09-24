9e063c2cdc7219bdaf1425e29eb97f798b838251
carrotwaxr <carrotwaxr@users.noreply.github.com>
2026-09-24 03:53:52 -0700

    Revert "test: client type-error probe (to be dropped)"
    This reverts commit 5943962c1c40adff415dd791197472822dfd499c.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,8 +4,6 @@ import App from "./App";
 import "./themes/fonts";
 import "./index.css";
 
-const _probe: number = "x";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
